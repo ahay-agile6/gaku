11facc91f20558fc739041fc4c77e4f62f5e5a13
lightning <aurora.hay@agile6.com>
2019-05-13 19:41:31 -0700

    Update students.rb

--- sample/db/sample/gaku/students.rb
+++ sample/db/sample/gaku/students.rb
@@ -5,8 +5,8 @@ say 'Creating predefined students ...'.yellow
 students = [
   { name: 'Anonime', surname: 'Anon', birth_date: Date.new(1982, 1, 1), enrollment_status_code: @enrollment_status },
   { name: 'Amon', surname: 'Tobin', birth_date: Date.new(1983, 1, 1), enrollment_status_code: @enrollment_status },
-  { name: '零', surname: '影月', enrollment_status_code: @enrollment_status },
-  { name: 'サニー', surname: 'スノー', enrollment_status_code: @enrollment_status }
+  { name: '零', surname: '影月', birth_date: Date.new(1982, 1, 1), enrollment_status_code: @enrollment_status },
+  { name: 'サニー', surname: 'スノー', birth_date: Date.new(1982, 1, 1), enrollment_status_code: @enrollment_status }
 ]
 
 create_student_with_full_info(@john_doe)
